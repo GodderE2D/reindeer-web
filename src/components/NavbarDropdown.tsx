a0b679873483a754f8076d1bd6f7497d896f4c79
GodderE2D <55918888+GodderE2D@users.noreply.github.com>
2023-08-15 16:24:09 +0800

    fix(NavbarDropdown): respect newTab option

--- src/components/NavbarDropdown.tsx
+++ src/components/NavbarDropdown.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 import { Fragment } from "react";
 import { HiBars3 } from "react-icons/hi2";
 
-export default function NavbarDropdown({ paths }: { paths: { path: string; name: string }[] }) {
+export default function NavbarDropdown({ paths }: { paths: { path: string; name: string; newTab?: boolean }[] }) {
   return (
     <Menu as="nav" className="relative inline-block text-left">
       <div>
@@ -23,15 +23,25 @@ export default function NavbarDropdown({ paths }: { paths: { path: string; name:
         leaveTo="transform opacity-0 scale-95"
       >
         <Menu.Items className="bg- absolute left-0 mt-2 w-56 origin-top-left divide-y divide-slate-700 rounded-md bg-slate-800 shadow-lg ring-opacity-5 focus:outline-none">
-          <div className="px-1 py-1 ">
-            {paths.map(({ path, name }) => (
+          <div className="px-1 py-1">
+            {paths.map(({ path, name, newTab }) => (
               <Menu.Item key={path}>
-                <Link
-                  href={path}
-                  className="group flex w-full items-center rounded-md px-2 py-2 text-sm text-slate-300 hover:bg-slate-700"
-                >
-                  {name}
-                </Link>
+                {newTab ? (
+                  <a
+                    href={path}
+                    target="_blank"
+                    className="group flex w-full items-center rounded-md px-2 py-2 text-sm text-slate-300 hover:bg-slate-700"
+                  >
+                    {name}
+                  </a>
+                ) : (
+                  <Link
+                    href={path}
+                    className="group flex w-full items-center rounded-md px-2 py-2 text-sm text-slate-300 hover:bg-slate-700"
+                  >
+                    {name}
+                  </Link>
+                )}
               </Menu.Item>
             ))}
           </div>
